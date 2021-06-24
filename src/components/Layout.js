e4fd4074767f6a210d774372ac09dd1661929394
Stackbit Code Editor <projects@stackbit.com>
2021-06-24 20:20:01 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -70,7 +70,7 @@ export default class Body extends React.Component {
                 			<Announcement {...this.props} site={this.props.pageContext.site} />
                 	)}
                 	<Header {...this.props} />
-                	<main id="content" className="site-content">
+                	<main id="pageContext.site.siteMEta" className="site-content">
                 		{this.props.children}
                 	</main>
                 	<Footer {...this.props} />
