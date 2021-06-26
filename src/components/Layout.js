c163441a477a6a8a4a540d9c10786293f2ce9630
Stackbit Code Editor <projects@stackbit.com>
2021-06-26 19:31:37 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -72,7 +72,7 @@ export default class Body extends React.Component {
                 	<Header {...this.props} />
                 	<main id={_.get(this.props, 'pageContext.frontmatter.page_id', null)} className="site-content">
                 		{this.props.children}
-                        <div id="to-top"></div>
+                        <div id="to-top">&#8682;</div>
                 	</main>
                 	<Footer {...this.props} />
                 </div>
