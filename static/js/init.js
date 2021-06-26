0e0cb57b6ea9fdcce37fba663789263eb6bdf0e0
Stackbit Code Editor <projects@stackbit.com>
2021-06-26 20:07:18 +0000

    init.js: changed

--- static/js/init.js
+++ static/js/init.js
@@ -101,6 +101,18 @@ window.addToTopHandler = function() {
     }
 };
 
+window.addToTopHandler = function() {
+    const toTop = document.querySelector('#to-top');
+    if(toTop) {
+        window.scroll({
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth'
+        });
+
+        toTop.addEventListener('click', announcementCloseHandler, false);
+    }
+};
 
 
 window.removeAnnouncementHandlers = function() {
